56d2b73ff3787f5374d1c0046afe53710a3e9391
ArseneDi0p <arsenelp.ytb@gmail.com>
2026-05-04 00:39:35 +0200

    Change 04/05 00:40
    neige ok

---
+++ Assets/Scripts/SnowSystem.cs
@@ -0,0 +1,45 @@
+using UnityEngine;
+using System.Collections;
+
+public class SnowSystem : MonoBehaviour
+{
+    private ParticleSystem myParticles;
+    private Transform playerTransform;
+    float elapsed;
+    float duration;
+    float currentX;
+    float result;
+    float t;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        myParticles = GetComponent<ParticleSystem>();
+        playerTransform = FindFirstObjectByType<FlowerSystem>().GetComponent<Transform>();
+        StartCoroutine(Wind());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var emission = myParticles.emission;
+        emission.rateOverTime = playerTransform.position.x + 10;
+    }
+
+    IEnumerator Wind()
+    {
+        Debug.Log("wind start");
+        elapsed = 0f;
+        duration = 3f;
+
+        while (elapsed < duration)
+        {
+            elapsed += Time.deltaTime;
+            t = elapsed / duration;
+            var Velocity = myParticles.velocityOverLifetime;
+            currentX = Velocity.x.constant;
+            result = Mathf.Lerp(currentX, playerTransform.position.x, t);
+            Velocity.x = new ParticleSystem.MinMaxCurve(result);
+            yield return null;
+        }
+    }
+}
