b57f3898b2547a0d952c2197799d9bcc7977a51e
Firkuss <firkusblaze@gmail.com>
2026-05-05 17:56:30 +0200

    Change 05/05 17:56
    Ascenseur presque ok
    Feu à revoir !!

--- Assets/Scripts/Elevator/FireLauncher.cs
+++ Assets/Scripts/Elevator/FireLauncher.cs
@@ -6,11 +6,19 @@ public class FireLauncher : MonoBehaviour
     [SerializeField]
     private GameObject Fire1;
     [SerializeField]
+    private Animator Fire1Anim;
+    [SerializeField]
     private GameObject Fire2;
+    [SerializeField]
+    private Animator Fire2Anim;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         StartCoroutine(Fire());
+        Fire1Anim.SetBool("LauncheFire", true);
+        Fire1Anim.SetBool("StopFire", false);
+        Fire2Anim.SetBool("LauncheFire", false);
+        Fire2Anim.SetBool("StopFire", true);
     }
 
     // Update is called once per frame
@@ -23,13 +31,23 @@ public class FireLauncher : MonoBehaviour
     {
         Fire1.SetActive(false);
         Fire2.SetActive(true);
+        Fire2Anim.SetBool("StopFire", false);
+        Fire2Anim.SetBool("LaunchFire", true);
         yield return new WaitForSeconds(3);
+        Fire2Anim.SetBool("LaunchFire", false);
+        Fire2Anim.SetBool("StopFire", true);
+        yield return new WaitForSeconds(1f);
         Fire1.SetActive(false);
         Fire2.SetActive(false);
         yield return new WaitForSeconds(1.5f);
         Fire1.SetActive(true);
         Fire2.SetActive(false);
+        Fire1Anim.SetBool("LaunchFire", true);
+        Fire1Anim.SetBool("StopFire", false);
         yield return new WaitForSeconds(3);
+        Fire1Anim.SetBool("LaunchFire", false);
+        Fire1Anim.SetBool("StopFire", true);
+        yield return new WaitForSeconds(1f);
         Fire1.SetActive(false);
         Fire2.SetActive(false);
         yield return new WaitForSeconds(1.5f);
