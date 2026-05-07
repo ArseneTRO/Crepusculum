42c824e1f2274e2d3c20e158fbf4c7d8a6181d36
ArseneTRO <tylio.taraud0@hotmail.com>
2026-05-07 16:59:53 +0200

    Change 07/05 16:59
    Lvl final crée

---
+++ Assets/Scripts/Environnement/DashableWall1.cs
@@ -0,0 +1,59 @@
+using UnityEngine;
+using UnityEngine.Splines.ExtrusionShapes;
+using System.Collections;
+using Unity.VisualScripting;
+
+public class DestructableWall : MonoBehaviour
+{
+    [SerializeField]
+    private BoxCollider2D myCollider; 
+    [SerializeField]
+    private BoxCollider2D playerBox;
+    [SerializeField]
+    private CircleCollider2D playercircle;
+    public PlayerMovement player;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            if (player.isDashing)
+            {
+                StartCoroutine(PassThroughWall());
+            }
+        }
+    }
+    void OnTriggerEnter2D (Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            if (player.isDashing)
+            {
+                StartCoroutine(PassThroughWall());
+            }
+        }
+    }
+
+
+    IEnumerator PassThroughWall()
+    {
+        print("Cible détruite");
+        Physics2D.IgnoreCollision(playerBox, myCollider, true);
+        Physics2D.IgnoreCollision(playercircle, myCollider, true);
+        yield return new WaitForSeconds(0.5f);
+        
+
+    }
+}
